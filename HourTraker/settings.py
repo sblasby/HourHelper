c1c7a3670cc559e2904227c89efeb04c5f0113f5
Simon Blasby <simon.blasby@gmail.com>
2023-09-17 22:39:14 -0400

    Debug to False

--- HourTraker/settings.py
+++ HourTraker/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3)+jbp&y0boa*@wmxz^x$qg*8y7(p*#$7c-(fwelcaj02ci-l5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-# ALLOWED_HOSTS = []
+#ALLOWED_HOSTS = []
 ALLOWED_HOSTS = ['trackhours.azurewebsites.net']
 
 
