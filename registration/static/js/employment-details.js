29ad99854d46d410b39d2ab8b3a40352cf852214
Simon Blasby <simon.blasby@gmail.com>
2023-09-25 15:48:18 -0400

    wage field only required when box is checked

--- registration/static/js/employment-details.js
+++ registration/static/js/employment-details.js
@@ -16,12 +16,15 @@ function Listeners(){
 
 function BoxClicked(name, field){
     const extra_details = document.getElementById(name + '-details')
+    const wage_field = document.getElementById(name + '-wage')
 
     if (field.checked){
         extra_details.style.display = 'block'
+        wage_field.setAttribute('required', 'required')
     }
     else{
         extra_details.style.display = 'none'
+        wage_field.removeAttribute('required')
     }
 
 }
